3236b7ac9baabde192dac9741635badd282cb43c
a10038830@gmail.com <e124232456>
2017-06-19 23:08:26 +0800

    修改
    第一集買了一本_其他都沒買_期望價格應為100X1_100元 改為 test_第一集買了一本_其他都沒買_期望價格應為100x1_100元

--- src/test/kotlin/ShoppingCartTest.kt
+++ src/test/kotlin/ShoppingCartTest.kt
@@ -4,7 +4,7 @@ import kotlin.test.assertEquals
 class ShoppingCartTest {
 
     @Test
-    fun 第一集買了一本_其他都沒買_期望價格應為100X1_100元() {
+    fun test_第一集買了一本_其他都沒買_期望價格應為100x1_100元() {
 
         val books = arrayListOf(Book(name = "哈利波特第一集", quantity = 1, salePrice = 100.0))
 
