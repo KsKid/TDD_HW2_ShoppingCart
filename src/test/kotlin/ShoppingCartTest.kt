b73208a849c5fb93a8f6ed4f4cdd20a23ef36a36
a10038830@gmail.com <e124232456>
2017-06-19 22:43:02 +0800

    第一次測試紅燈

---
+++ src/test/kotlin/ShoppingCartTest.kt
@@ -0,0 +1,19 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ShoppingCartTest {
+
+    @Test
+    fun 第一集買了一本_其他都沒買_期望價格應為100X1_100元() {
+
+        val books = arrayListOf(Book(name = "哈利波特第一集", quantity = 1, salePrice = 100.0))
+
+        val target = ShoppingCartTest(books)
+
+        val expected = 100.0
+
+        val actual = target.getSubtotal()
+
+        assertEquals(expected, actual)
+    }
+}
