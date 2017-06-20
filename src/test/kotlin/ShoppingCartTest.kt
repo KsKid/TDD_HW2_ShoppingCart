ea22dd345d04d5e63badfa98ad194e2eccd5da5b
a10038830@gmail.com <e124232456>
2017-06-21 01:27:51 +0800

    新增測試案例 : test_一二集各買了一本_第三集買了兩本_期望價格應為100x3x0_9加100_370元

--- src/test/kotlin/ShoppingCartTest.kt
+++ src/test/kotlin/ShoppingCartTest.kt
@@ -90,4 +90,22 @@ class ShoppingCartTest {
 
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun test_一二集各買了一本_第三集買了兩本_期望價格應為100x3x0_9加100_370元() {
+
+        val books = arrayListOf(
+                Book(name = "哈利波特第一集", quantity = 1, salePrice = 100.0),
+                Book(name = "哈利波特第二集", quantity = 1, salePrice = 100.0),
+                Book(name = "哈利波特第三集", quantity = 2, salePrice = 100.0)
+        )
+
+        val target = ShoppingCart(books)
+
+        val expected = 370.0
+
+        val actual = target.getSubtotal()
+
+        assertEquals(expected, actual)
+    }
 }
